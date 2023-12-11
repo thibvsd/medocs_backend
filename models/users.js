971606a042e987ed23216f71bfd1126d56ac2b02
Thibaud Veyssade <tveyssade@gmail.com>
2023-12-11 17:41:32 +0100

    implantation des modules

---
+++ models/users.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  username: String,
+  password: String,
+  email: String,
+  age: Number,
+  weight: Number,
+  token: String,
+});
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = User;
