1a7b90055e1af574e906f947306aa3b553740a76
Marie de Breuvand <marie.carette@gmail.com>
2023-12-12 16:23:43 +0100

    routes favorites et articles

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,9 +1,80 @@
-var express = require('express');
+var express = require("express");
+const User = require("../../../medidoc_old copy/backend/medocs_backend/models/users");
 var router = express.Router();
 
-/* GET favorites page. */
-router.get('/', function(req, res, next) {
-  res.render('favorites', { title: 'Express' });
+// Supprime un favori par id
+router.delete("/:token/:drug_id", async (req, res) => {
+  try {
+    const userToken = req.params.token;
+    const drugId = req.params.drug_id;
+    // Recherche de l'utilisateur par token
+    const user = await User.findOne({ token: userToken });
+    if (!user) {
+      return res.json({ result: false, error: "User not found" });
+    }
+    // Suppression de l'ID du médicament dans le tableau des favoris
+    user.favorites = user.favorites.filter(
+      (favoriteId) => favoriteId.toString() !== drugId
+    );
+    // Enregistrement de l'utilisateur mis à jour
+    await user.save();
+    res.json({ result: true, user: user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ result: false, error: "Internal Server Error" });
+  }
+});
+
+// Ajoute un favori
+router.post("/:token/addFavorite/:drug_id", async (req, res) => {
+  try {
+    const userToken = req.params.token;
+    const drugId = req.params.drug_id;
+    // Recherche de l'utilisateur par token
+    const user = await User.findOne({ token: userToken });
+    if (!user) {
+      return res.json({ result: false, error: "User not found" });
+    }
+    // Vérification si le médicament existe déjà dans les favoris
+    const isDrugAlreadyFavorited = user.favorites.some(
+      (favoriteId) => favoriteId.toString() === drugId
+    );
+    if (isDrugAlreadyFavorited) {
+      return res.json({ result: false, error: "Drug already in favorites" });
+    }
+    // Ajout de l'ID du médicament dans le tableau des favoris
+    user.favorites.push(drugId);
+    // Enregistrement de l'utilisateur mis à jour
+    await user.save();
+    res.json({ result: true, user: user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ result: false, error: "Internal Server Error" });
+  }
+});
+
+
+// Vérifie si un favori est présent
+router.get("/:token/isFavorite/:drug_id", async (req, res) => {
+  try {
+    const userToken = req.params.token;
+    const drugId = req.params.drug_id;
+
+    // Recherche de l'utilisateur par token
+    const user = await User.findOne({ token: userToken });
+
+    if (!user) {
+      return res.json({ result: false, error: "User not found" });
+    }
+
+    // Vérification si le médicament existe déjà dans les favoris
+    const isDrugFavorite = user.favorites.some(favoriteId => favoriteId.toString() === drugId);
+
+    res.json({ result: true, isFavorite: isDrugFavorite });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ result: false, error: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
